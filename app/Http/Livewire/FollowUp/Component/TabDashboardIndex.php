ade8d7540225c275ecc74476853fad2efd2f2a82
dandhi <dandhiramadan122098@gmail.com>
2023-08-02 14:50:37 +0700

    statisktik tab

---
+++ app/Http/Livewire/FollowUp/Component/TabDashboardIndex.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Livewire\FollowUp\Component;
+
+use App\Models\Files;
+use Livewire\Component;
+use App\Models\WorkStep;
+use App\Models\Instruction;
+use Livewire\WithPagination;
+
+class TabDashboardIndex extends Component
+{
+    public $dataCountNewSpk;
+    public $dataCountRejectSpk;
+    public $dataCountRunningSpk;
+    public $dataCountHoldSpk;
+    public $dataCountCancelSpk;
+    public $dataCountCompleteSpk;
+    public $dataCountAllSpk;
+
+    protected $listeners = ['indexRender' => 'renderIndex'];
+
+    public function renderIndex()
+    {
+        $this->render();
+    }
+    
+    public function render()
+    {
+        $this->dataCountNewSpk = WorkStep::where('work_step_list_id', 1)
+                ->where('state_task', 'Running')
+                ->where('status_task', 'Process')
+                ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Deleted', 'Training Program'])
+                ->whereIn('status_id', [1, 2])
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountRejectSpk = WorkStep::where('work_step_list_id', 1)
+                ->where('state_task', 'Running')
+                ->whereIn('status_task', ['Reject', 'Reject Requirements'])
+                ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Deleted', 'Training Program'])
+                ->whereIn('status_id', [3, 22])
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountRunningSpk = WorkStep::where('work_step_list_id', 1)
+                ->where('state_task', 'Running')
+                ->where('status_task', 'Process')
+                ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Deleted', 'Training Program'])
+                ->whereIn('status_id', [1, 2, 23])
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountHoldSpk = WorkStep::where('work_step_list_id', 1)
+                ->whereIn('spk_status', ['Hold', 'Hold Waiting Qty QC', 'Hold RAB'])
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountCancelSpk = WorkStep::where('work_step_list_id', 1)
+                ->where('spk_status', 'Cancel')
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountCompleteSpk = WorkStep::where('work_step_list_id', 1)
+                ->where('spk_status', 'Selesai')
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        $this->dataCountAllSpk = WorkStep::where('work_step_list_id', 1)
+                ->whereNotIn('spk_status', ['Selesai', 'Training Program'])
+                ->whereHas('instruction', function ($query) {
+                    $query->where('group_priority', '!=', 'child')
+                         ->orWhereNull('group_priority');
+                })->orderBy('shipping_date', 'asc')
+                ->with(['status', 'job', 'workStepList', 'instruction'])
+                ->count();
+
+        
+
+        return view('livewire.follow-up.component.tab-dashboard-index')
+
+        ->extends('layouts.app')
+        ->section('content')
+        ->layoutData(['title' => 'Dashboard']);
+    }
+}
+
