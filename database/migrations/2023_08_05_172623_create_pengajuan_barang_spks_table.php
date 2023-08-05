6558aa8a494ba5503afb6e8913bb2ee8b3f87216
dandhi <dandhiramadan122098@gmail.com>
2023-08-06 00:33:02 +0700

    db

--- database/migrations/2023_08_05_172623_create_pengajuan_barang_spks_table.php
+++ database/migrations/2023_08_05_172623_create_pengajuan_barang_spks_table.php
@@ -15,6 +15,21 @@ class CreatePengajuanBarangSpksTable extends Migration
     {
         Schema::create('pengajuan_barang_spks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('instruction_id');
+            $table->foreign('instruction_id')->references('id')->on('instructions')->onDelete('cascade');
+            $table->unsignedBigInteger('work_step_list_id');
+            $table->foreign('work_step_list_id')->references('id')->on('work_step_lists');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('tgl_pengajuan')->nullable();
+            $table->date('tgl_target_datang')->nullable();
+            $table->date('tgl_tersedia')->nullable();
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->string('state')->nullable();
+            $table->string('nama_barang')->nullable();
+            $table->string('qty_barang')->nullable();
+            $table->longtext('keterangan')->nullable();
             $table->timestamps();
         });
     }
