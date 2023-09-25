91711af2e48f1d5b5b1eeec15b95cf03ea341d1e
domtechcode <domcorporationtech@gmail.com>
2023-09-25 14:10:34 +0700

    manage

--- app/Http/Livewire/Penjadwalan/Component/OperatorDashboardIndex.php
+++ app/Http/Livewire/Penjadwalan/Component/OperatorDashboardIndex.php
@@ -109,7 +109,7 @@ class OperatorDashboardIndex extends Component
             $dataDetailWorkStep = WorkStep::where('work_step_list_id', $this->worksteplistSelected)
                 ->where('user_id', $this->userSelected)
                 ->whereIn('state_task', ['Running', 'Not Running'])
-                ->whereIn('status_task', ['Pending Approved', 'Process'])
+                ->whereIn('status_task', ['Pending Approved', 'Process', 'Waiting'])
                 ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Hold Qc', 'Failed Waiting Qty QC', 'Deleted', 'Acc', 'Close PO', 'Training Program'])
                 ->where(function ($query) {
                     $searchTerms = '%' . $this->dijadwalkanSelected . '%';
@@ -161,7 +161,7 @@ class OperatorDashboardIndex extends Component
         if ($this->userSelected == 'all') {
             $dataDetailWorkStep = WorkStep::where('work_step_list_id', $this->worksteplistSelected)
                 ->whereIn('state_task', ['Running', 'Not Running'])
-                ->whereIn('status_task', ['Pending Approved', 'Process'])
+                ->whereIn('status_task', ['Pending Approved', 'Process', 'Waiting'])
                 ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Hold Qc', 'Failed Waiting Qty QC', 'Deleted', 'Acc', 'Close PO', 'Training Program'])
                 ->where(function ($query) {
                     $searchTerms = '%' . $this->dijadwalkanSelected . '%';
@@ -195,7 +195,7 @@ class OperatorDashboardIndex extends Component
             $dataDetailWorkStep = WorkStep::where('work_step_list_id', $this->worksteplistSelected)
                 ->where('user_id', $this->userSelected)
                 ->whereIn('state_task', ['Running', 'Not Running'])
-                ->whereIn('status_task', ['Pending Approved', 'Process'])
+                ->whereIn('status_task', ['Pending Approved', 'Process', 'Waiting'])
                 ->whereNotIn('spk_status', ['Hold', 'Cancel', 'Hold', 'Hold RAB', 'Hold Waiting Qty QC', 'Hold Qc', 'Failed Waiting Qty QC', 'Deleted', 'Acc', 'Close PO', 'Training Program'])
                 ->where(function ($query) {
                     $searchTerms = '%' . $this->dijadwalkanSelected . '%';
