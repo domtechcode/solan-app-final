302f78f0720584cd646337ae680c9708b5abdcb8
domtechcode <domcorporationtech@gmail.com>
2023-10-19 20:17:20 +0700

    late kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('update:worksteps')->dailyAt('23:40');
+        $schedule->command('update:worksteps')->dailyAt('23:40');
     }
 
     /**
